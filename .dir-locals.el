6fb690c87dbbb3fbe6298865134cbde64f47b909
Peter Byrne <byornski@gmail.com>
2018-04-18 14:27:13 +0100

    Select default tags file

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,6 +7,7 @@
 	      (eval . (message "Project directory set to `%s'." my-project-path))   ;;;Print debug message
 	      (eval . (setq default-directory my-project-path))                     ;;;Set main folder (for compile to work)
 	      (eval . (setq flycheck-gfortran-include-path (list (concat my-project-path "obj/linux_x86_64_gfortran7.0")))) ;;; Add build path to include
+	      (eval . (setq tags-file-name "TAGS"))             ;;; Tag file name -- generate with ctags -e -R Source/
 	      (flycheck-gfortran-language-standard . "f2008")   ;;;Set fortran standard to warn/check
 	      (compile-command . "make"))))  ;;;Set build command (want to stop on first error so no -k)
 
