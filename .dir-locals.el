7c383cb7ec5385306c83c1537648171dd07576f6
Peter Byrne <byornski@gmail.com>
2018-04-25 15:42:54 +0100

    Added more fortan warnings

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,9 +5,29 @@
                        (let ((d (dir-locals-find-file ".")))
                          (if (stringp d) d (car d))))))
 	      (eval . (message "Project directory set to `%s'." my-project-path))   ;;;Print debug message
-	      (eval . (setq default-directory my-project-path))                     ;;;Set main folder (for compile to work)
-	      (eval . (setq flycheck-gfortran-include-path (list (concat my-project-path "obj/linux_x86_64_gfortran7.0")))) ;;; Add build path to include
-	      (eval . (setq tags-file-name "TAGS"))             ;;; Tag file name -- generate with ctags -e -R Source/
-	      (flycheck-gfortran-language-standard . "f2003")   ;;;Set fortran standard to warn/check
-	      (compile-command . "make"))))  ;;;Set build command (want to stop on first error so no -k)
+
+	      
+	      ;;; Set main project directory
+	      (eval . (setq default-directory my-project-path)) 
+	      ;;;Set build command (want to stop on first error so no -k)
+	      (compile-command . "make")  
+
+	      ;;; Flycheck
+	      ;;; Set fortran standard to warn/check
+	      (flycheck-gfortran-language-standard . "f2003")
+	      (flycheck-gfortran-warnings . ("all" "extra" "implicit-procedure" "implicit-interface"
+					     "use-without-only"))
+	      
+	      ;;; Flycheck error limit 
+	      (eval . (setq flycheck-checker-error-threshold 1000))
+	      ;;; Add build path to include directory for inter-module checking
+	      (eval . (setq flycheck-gfortran-include-path
+			    (list (concat my-project-path "obj/linux_x86_64_gfortran7.0")))) 
+	      
+	      
+	      ;;; Tags file stuff
+	      (eval . (setq tags-file-name "TAGS")) 
+
+	      
+	      )))
 
